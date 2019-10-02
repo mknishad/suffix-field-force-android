1880466d4f675f6ee6ff242e2c27d222000f7f5f
Monir Hossain <mknishad1@ymail.com>
2019-10-02 18:18:15 +0600

    add task details activity

---
+++ app/src/main/java/com/suffix/fieldforce/activity/TaskDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.suffix.fieldforce.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.suffix.fieldforce.R
+
+class TaskDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_details)
+    }
+}
