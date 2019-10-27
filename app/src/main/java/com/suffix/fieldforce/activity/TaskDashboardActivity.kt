88f79c94ff7b8af3c50d34b12c63ab1bb9b4f1c3
Monir Hossain <mknishad1@ymail.com>
2019-10-28 00:46:46 +0600

    add and design task summary activity

---
+++ app/src/main/java/com/suffix/fieldforce/activity/TaskDashboardActivity.kt
@@ -0,0 +1,13 @@
+package com.suffix.fieldforce.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.suffix.fieldforce.R
+
+class TaskDashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_dashboard)
+    }
+}
