60bd8b82a7cf2afa5bf84c576cff61b8b3d1ca25
smensulaiman <smensulaiman007@gmail.com>
2019-11-08 17:38:44 +0600

    Some Code Added

--- app/src/main/java/com/suffix/fieldforce/networking/MyFirebaseMessagingService.java
+++ app/src/main/java/com/suffix/fieldforce/networking/MyFirebaseMessagingService.java
@@ -7,41 +7,39 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Build;
 import android.util.Log;
-import android.util.Range;
-
 import androidx.core.app.NotificationCompat;
-
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.suffix.fieldforce.R;
-
 import java.util.Random;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
-        showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
-
+        try {
+            showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void showNotification(String title, String body) {
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         String NOTIFICATION_CHANNEL_ID = "com.suffix.fieldforce";
 
-        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.O){
-
-            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,"notification",notificationManager.IMPORTANCE_DEFAULT);
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "notification",
+                    notificationManager.IMPORTANCE_DEFAULT);
             notificationChannel.setDescription("Suffix Field Force");
             notificationChannel.enableLights(true);
             notificationChannel.setLightColor(Color.BLUE);
-            notificationChannel.setVibrationPattern(new long[]{0,1000,500,1000});
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
 
             notificationManager.createNotificationChannel(notificationChannel);
         }
 
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this,NOTIFICATION_CHANNEL_ID);
-
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
         notificationBuilder.setAutoCancel(true);
         notificationBuilder.setDefaults(Notification.DEFAULT_ALL);
         notificationBuilder.setWhen(System.currentTimeMillis());
@@ -49,14 +47,12 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         notificationBuilder.setContentTitle(title);
         notificationBuilder.setContentText(body);
         notificationBuilder.setContentInfo("info");
-
-        notificationManager.notify(new Random().nextInt(),notificationBuilder.build());
-
-     }
+        notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
+    }
 
     @Override
     public void onNewToken(String s) {
         super.onNewToken(s);
-        Log.d("NewToken",s);
+        Log.d("NewToken", s);
     }
 }
