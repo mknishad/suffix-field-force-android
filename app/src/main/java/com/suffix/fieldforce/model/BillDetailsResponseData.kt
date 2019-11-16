c62d5afda9d50b0f4f70effd175518d7b36c0b5a
Monir Hossain <mknishad1@ymail.com>
2019-11-17 00:57:05 +0600

    resolve bill details model issue

--- app/src/main/java/com/suffix/fieldforce/model/BillDetailsResponseData.kt
+++ app/src/main/java/com/suffix/fieldforce/model/BillDetailsResponseData.kt
@@ -1,14 +1,13 @@
 package com.suffix.fieldforce.model
 
 import android.os.Parcelable
-import com.squareup.moshi.Json
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class BillDetailsResponseData(
-    @Json(name = "ResponseCode") var responseCode: String,
+    //@Json(name = "ResponseCode") var responseCode: String,
     var billDetailsObj: BillDetailsObj,
     var billListObj: BillListObj,
-    var billApproveObj: BillApproveObj,
-    @Json(name = "ResponseText") var responseText: String
+    var billApproveObj: BillApproveObj
+    //@Json(name = "ResponseText") var responseText: String
 ) : Parcelable
