e3a49f486c1e6f2fa38464ec5f9699f1db3b8198
Mohammad Solaiman <smensulaiman007@gmail.com>
2020-01-13 17:47:02 +0600

    New Layout Added

---
+++ app/src/main/java/com/suffix/fieldforce/activity/home/MainDashboardActivityTypeTwo.java
@@ -0,0 +1,16 @@
+package com.suffix.fieldforce.activity.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.suffix.fieldforce.R;
+
+public class MainDashboardActivityTypeTwo extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main_dashboard_type_two);
+  }
+}
