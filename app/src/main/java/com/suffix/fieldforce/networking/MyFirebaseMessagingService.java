fc5d0d5340506eb04115fb396875246ef0fb4ddb
smensulaiman <smensulaiman007@gmail.com>
2020-01-22 21:54:57 +0600

    fix #8

--- app/src/main/java/com/suffix/fieldforce/networking/MyFirebaseMessagingService.java
+++ app/src/main/java/com/suffix/fieldforce/networking/MyFirebaseMessagingService.java
@@ -3,7 +3,9 @@ package com.suffix.fieldforce.networking;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
@@ -12,45 +14,64 @@ import androidx.core.app.NotificationCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.suffix.fieldforce.R;
+import com.suffix.fieldforce.activity.MainActivity;
+
 import java.util.Random;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
-    private Uri uri = null;
+
+    private Uri uri = Uri.parse("android.resource://"+getPackageName()+"/raw/notification_sound.mp3");
+    private String NOTIFICATION_CHANNEL_ID = "com.suffix.fieldforce";
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
+        String message = remoteMessage.getData().get("message");
+        String imageUri = remoteMessage.getData().get("image");
+        String title = remoteMessage.getData().get("title");
+        String action = remoteMessage.getData().get("click_action");
+
         try {
-            showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+            showNotification(title, message);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     private void showNotification(String title, String body) {
-        uri = Uri.parse("android.resource://"+getPackageName()+"/raw/notification_sound.mp3");
+
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        String NOTIFICATION_CHANNEL_ID = "com.suffix.fieldforce";
+
+        Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
+        notificationIntent.putExtra("BODY", body);
+        notificationIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        notificationIntent.setAction(Intent.ACTION_MAIN);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        PendingIntent resultIntent = PendingIntent.getActivity(getApplicationContext(),  (int) (Math.random() * 100), notificationIntent, 0);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        notificationBuilder.setAutoCancel(true);
+        notificationBuilder.setDefaults(Notification.FLAG_AUTO_CANCEL | Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_SOUND);
+        notificationBuilder.setWhen(System.currentTimeMillis());
+        notificationBuilder.setSmallIcon(R.drawable.message);
+        notificationBuilder.setContentTitle(title);
+        notificationBuilder.setContentText(body);
+        notificationBuilder.setContentInfo("info");
+        notificationBuilder.setContentIntent(resultIntent);
 
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
             NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "notification",
-                    notificationManager.IMPORTANCE_DEFAULT);
-            notificationChannel.setDescription("Suffix Field Force");
+                notificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription(body);
             notificationChannel.enableLights(true);
             notificationChannel.setLightColor(Color.BLUE);
             notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
 
             notificationManager.createNotificationChannel(notificationChannel);
         }
-
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
-        notificationBuilder.setAutoCancel(true);
-        notificationBuilder.setDefaults(Notification.DEFAULT_ALL);
-        notificationBuilder.setWhen(System.currentTimeMillis());
-        notificationBuilder.setSmallIcon(R.drawable.message);
-        notificationBuilder.setContentTitle(title);
-        notificationBuilder.setContentText(body);
-        notificationBuilder.setContentInfo("info");
-        notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
+        assert notificationManager != null;
+        notificationManager.notify((int)System.currentTimeMillis(), notificationBuilder.build());
     }
 
     @Override
