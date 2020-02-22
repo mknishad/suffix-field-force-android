0ddb575b1cfacabf646f4fdea3d6048d08446abd
Monir Hossain <mknishad1@ymail.com>
2020-02-22 18:26:29 +0600

    get location from preference instead of smart location

--- app/src/main/java/com/suffix/fieldforce/MyApplication.java
+++ app/src/main/java/com/suffix/fieldforce/MyApplication.java
@@ -5,20 +5,12 @@ import android.app.Application;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.google.firebase.database.FirebaseDatabase;
 
-import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
-
 public class MyApplication extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
     FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     Fresco.initialize(this);
-
-    CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
-        .setDefaultFontPath("font/circulate.ttf")
-        .setFontAttrId(R.attr.fontPath)
-        .build()
-    );
 
   }
 }
