235df1c095dafd6fc8857906798418f81ae1ae60
smensulaiman <smensulaiman007@gmail.com>
2020-11-27 16:16:56 +0600

    Design Added

---
+++ app/src/main/java/com/suffix/fieldforce/activity/abul/SaleActivity.java
@@ -0,0 +1,36 @@
+package com.suffix.fieldforce.activity.abul;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.suffix.fieldforce.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SaleActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_sale);
+
+    Spinner spinner = findViewById(R.id.spinnerUsers);
+
+    ArrayAdapter<String> myAdapter = new CustomArrayAdapter(this, R.layout.spinner_item, getList());
+    spinner.setAdapter(myAdapter);
+
+  }
+
+  private List<String> getList() {
+    String[] years = {"1996","1997","1998","1998"};
+    List<String> myList = new ArrayList<>();
+    for(String value : years){
+      myList.add(value);
+    }
+    return myList;
+  }
+}
